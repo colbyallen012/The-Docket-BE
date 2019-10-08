b97a8009d108d26b5731bdaef6190333cf886784
Colby Allen <colbyallen012@gmail.com>
2019-10-08 09:34:24 -0500

    add post functionality to grocery list

--- app.js
+++ app.js
@@ -20,4 +20,25 @@ app.get('/api/v1/groceryLists', (request, response) => {
     .catch(error => response.status(500).json({error}))
 });
 
+app.post('/api/v1/groceryLists', (request, response) => {
+  const groceryList = request.body;
+  for(let requiredParameter of ['list_title']) {
+    if(!groceryList[requiredParameter]) {
+      return response
+        .status(422)
+        .send({error: `Expected format: {name: <String>}. You're missing the ${requiredParameter} property.`})
+    }
+  }
+  database('groceryLists').where('list_title', groceryList.list_title).select()
+    .then(existingList => {
+      if(!existingList.length) {
+        database('groceryLists').insert(groceryList, 'id')
+          .then(groceryList => response.status(201).json({list_name: groceryList[0]}))
+          .catch(error => response.status(500).json({error}))
+      } else {
+        response.status(409).json(`${groceryList.name} already exists.`)
+      }
+    })
+});
+
 module.exports = app;
