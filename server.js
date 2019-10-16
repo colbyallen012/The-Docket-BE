4f05678fd4b0fd8f989d8f18e3b5a94272bfc7b9
Colby Allen <colbyallen012@gmail.com>
2019-10-16 11:14:45 -0600

    update port for local host

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-// const express = require('express');
-// const app = express();
-
 const app = require('./app')
 
 app.set('port', process.env.PORT || 3001);
-// app.locals.title = 'The Docket';
-
-// app.get('/', (request, response) => {
-//   response.send(`What's on The Docket?`);
-// });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
